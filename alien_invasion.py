2187b3567cd01d7183cf6e69bc5f745f73addf55
ixuanyu <1251244823@qq.com>
2024-11-29 20:51:58 +0800

    加入ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,6 @@
 import sys, pygame
+from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -6,8 +8,11 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
     
     def run_game(self):
         """开始游戏的主循环"""
@@ -17,8 +22,10 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            self.screen.fill(self.settings.bg_color)
             # 让最近绘制的屏幕可见
             pygame.display.flip()
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
