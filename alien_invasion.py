c336f4453c4b4c10fc68943b0052bf2391c319cd
ixuanyu <1251244823@qq.com>
2024-11-30 07:25:00 +0800

    飞船左右移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,22 +9,45 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode(self.settings.screen_width, self.settings.screen_height)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
     
+    def _check_event(self):
+        # 侦听键盘和鼠标事件
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+
+                    
+    
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新的屏幕"""
+        # 设置背景颜色
+        self.screen.fill(self.settings.bg_color)
+        # 绘制飞船
+        self.ship.blitme()
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+         
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 侦听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            self.screen.fill(self.settings.bg_color)
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._check_event()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
 
 if __name__ == '__main__':
