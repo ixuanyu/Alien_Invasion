f1232309e125e6f9a8c45b39fedc1a79ef70a34c
ixuanyu <1251244823@qq.com>
2024-12-02 21:01:29 +0800

    创建外星人舰队

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys, pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -18,10 +19,27 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._creat_fleet()
     
+    def _creat_fleet(self):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
+
+
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet) 
 
     def _check_keydown_events(self, event):
         """响应按下"""
@@ -30,9 +48,9 @@ class AlienInvasion:
                 self.ship.moving_right = True
             elif event.key == pygame.K_LEFT:
                 self.ship.moving_left = True
-            elif event.key == pygame.K_q:
+            elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
                 sys.exit()
-            elif event.key == pygame.K_BACKSPACE:
+            elif event.key == pygame.K_SPACE:
                 self._fire_bullet()
     
     def _check_keyup_events(self, event):
@@ -53,7 +71,15 @@ class AlienInvasion:
                 sys.exit()
             self._check_keydown_events(event)
             self._check_keyup_events(event)
-                    
+    
+    def _update_bullets(self):
+        """更新子弹位置并删除已消失的子弹"""
+        # 更新子弹位置
+        self.bullets.update()
+        # 删除已消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)        
     
     def _update_screen(self):
         """更新屏幕上的图像，并切换到新的屏幕"""
@@ -64,6 +90,7 @@ class AlienInvasion:
             bullet.draw_bullet()
         # 绘制飞船
         self.ship.blitme()
+        self.aliens.draw(self.screen)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
          
@@ -73,7 +100,7 @@ class AlienInvasion:
         while True:
             self._check_event()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
 
